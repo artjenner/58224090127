32d74534ceb76339965b4d013d599a77cede1588
artjenner <31381823+artjenner@users.noreply.github.com>
2017-08-27 16:40:35 +0700

    2-38

---
+++ artjen.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("58224090127\n");
+	printf("%-8s: %-13s\n","Thakdanai","Pankaw");
+	printf("22/March/1995\n");
+	printf("%119s\n","http://www.npru.ac.th");
+	printf("%65s\n","bye bye");
+	return 0;
+} 
